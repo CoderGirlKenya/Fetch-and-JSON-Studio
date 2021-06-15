023a0b1ba04d9a541ba867c9fca563825c4eb0c6
CoderGirlKenya <kenya78hami@gmail.com>
2021-06-14 20:09:02 -0500

    updated js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// TODO: add code here
+
+window.addEventListener("load", function(){
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        console.log(response);
+    response.json().then(function(json){
+      console.log(json);
+    
+      for(astronauts of json){
+        const container = document.getElementById("container");
+        container.innerHTML =
+        
+     };
+    });    
+  });
+});
+
